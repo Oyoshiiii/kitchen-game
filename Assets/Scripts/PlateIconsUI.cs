d05cb31d39b8e6eca869c2932c3e266fbe6d567a
yoshi <taradaevaks27@mail.ru>
2026-02-12 23:53:51 +0700

    настроены иконки тарелки, добавлен delivery counter

--- Assets/Scripts/PlateIconsUI.cs
+++ Assets/Scripts/PlateIconsUI.cs
@@ -8,6 +8,11 @@ public class PlateIconsUI : MonoBehaviour
     [SerializeField]
     private Transform iconTemplate;
 
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
     private void Start()
     {
         plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
@@ -20,10 +25,17 @@ public class PlateIconsUI : MonoBehaviour
 
     private void UpdateVisual()
     {
+        foreach(Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
         foreach(KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
         {
             Transform iconTransform = Instantiate(iconTemplate, transform);
-            iconTransform.Find("Image");
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconsSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
         }
     }
 }
